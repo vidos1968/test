108c5dbc83d51c4e6b5e34e9cb9a708712ab07be
vidos1968 <dima@vidoss.ru>
2018-01-10 10:23:17 +0300

    Izi modal add

--- js/common.js
+++ js/common.js
@@ -2,9 +2,9 @@ $(document).on('click', '.trigger', function (event) {
     event.preventDefault();
     // $('#modal').iziModal('setZindex', 99999);
     // $('#modal').iziModal('open', { zindex: 99999 });
-    $('#modal').iziModal('open');
+    $('#modal-demo').iziModal('open');
 });
 
 
 
-$("#modal").iziModal();
+$("#modal-demo").iziModal();
